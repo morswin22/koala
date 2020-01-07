d7455921ee917963712949bb4f98c2a113e2cbc5
morswin22 <xmorswinx@gmail.com>
2020-01-07 17:24:46 +0100

    Added P5 (caesar cipher)

---
+++ P5/caesar.py
@@ -0,0 +1,26 @@
+import re
+
+# wartości stałe
+alfabet = []
+for litera in "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUWVXYZŹŻ":
+    alfabet.append(litera)
+
+# wczytanie danych
+zaszyfrowane = input("Zaszyfrowany tekst: ").strip().upper()
+wyraz = input("Wyraz, który w nim występuje: ").strip().upper()
+
+# obliczenie
+odszyfrowane = []
+for n in range(1,len(alfabet)+1):
+    tekst = ""
+    for litera in zaszyfrowane:
+        indeks = alfabet.index(litera) - n
+        if indeks < 0:
+            indeks += len(alfabet)
+        tekst += alfabet[indeks]
+    if re.search(rf"{wyraz}", tekst, re.UNICODE):
+        odszyfrowane.append((str(n), tekst))
+
+# wypisanie wyniku
+for dane in odszyfrowane:
+    print(' '.join(dane))
